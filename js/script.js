4ef5d290278846de06ee3282d9044ee73b3f51b6
Gabriel Almeida <gabrielalmeida7258@gmail.com>
2024-11-12 21:11:16 -0300

    menu hambúrguer funcionando. Sem animações

---
+++ js/script.js
@@ -0,0 +1,15 @@
+const menu = document.getElementById("toggle-menu");
+
+menu.addEventListener("change", openMenu);
+
+function openMenu() {
+    const labelMenu = document.querySelector("#toggle-menu + label span");
+
+    document.body.classList.toggle("menu-open");
+
+    if (menu.checked) {
+        labelMenu.innerHTML = "close";
+    } else {
+        labelMenu.innerHTML = "menu";
+    };
+};
