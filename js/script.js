1075bb85763f7b0860bc6c2c2184dd14742daef5
Gabriel Almeida <gabrielalmeida7258@gmail.com>
2024-11-19 18:09:53 -0300

    modo escuro adicionado. Partindo para as Medias Queries

--- js/script.js
+++ js/script.js
@@ -1,6 +1,21 @@
 const menu = document.getElementById("toggle-menu");
-
 menu.addEventListener("change", openMenu);
+
+const btToggleTheme = document.querySelector(".menu-buttons.toggle-theme");
+btToggleTheme.addEventListener("click", toggleThemeMode);
+
+document.addEventListener("DOMContentLoaded", () => {
+    const body = document.body;
+
+    const darkModeEnabled = localStorage.getItem("dark-mode") === "true";
+
+    if (darkModeEnabled) {
+        body.classList.add("dark-theme");
+
+        const icon = document.querySelector(".top-menu .menu-buttons.toggle-theme .material-icons");
+        icon.innerHTML = "dark_mode";
+    };
+})
 
 function openMenu() {
     const labelMenu = document.querySelector("#toggle-menu + button label span");
@@ -11,5 +26,18 @@ function openMenu() {
         labelMenu.innerHTML = "close";
     } else {
         labelMenu.innerHTML = "menu";
+    };
+};
+
+function toggleThemeMode() {
+    const isDarkTheme = document.body.classList.toggle("dark-theme");
+
+    localStorage.setItem("dark-mode", isDarkTheme)
+
+    const icon = document.querySelector(".top-menu .menu-buttons.toggle-theme .material-icons");
+    if (document.body.classList.contains("dark-theme")) {
+        icon.innerHTML = "dark_mode";
+    } else {
+        icon.innerHTML = "wb_sunny";
     };
 };
